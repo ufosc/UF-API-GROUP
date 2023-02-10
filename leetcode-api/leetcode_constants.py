ab676c8bf465df90c885e4becf17c81a09581d53
cbloodsworth <christopherbloodsworth@gmail.com>
2023-02-10 12:17:15 -0500

    (feat) Basic scraping of LC website. No API functionality yet

---
+++ leetcode-api/leetcode_constants.py
@@ -0,0 +1,6 @@
+# On LC user page, div class for number of completed easy, medium, and hard
+DIFF_DIV_CLASS = "mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1"
+RANK_DIV_CLASS = "ttext-label-1 dark:text-dark-label-1 font-medium"
+RECENT_DIV_CLASS = "text-label-3 dark:text-dark-label-3 hidden whitespace-nowrap lc-md:inline"
+RECENT_PROBLEM_DIV_CLASS = "text-label-1 dark:text-dark-label-1 font-medium line-clamp-1"
+STREAK_DIV_CLASS = "font-medium text-label-2 dark:text-dark-label-2"
